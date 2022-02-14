d7fa3e6e153846342284d71876441be1741e5ed6
muhmmad.nabil.albadry@gmail.com <muhmmad.nabil.albadry@gmail.com>
2022-02-14 21:29:04 +0200

    The design

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -19,5 +19,10 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        binding.login.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
